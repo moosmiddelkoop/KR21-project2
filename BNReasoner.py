0cf6e75549815980ade4d550cdc4ea95737fd3c7
Moos <jimmemiddelkoop@gmail.com>
2022-12-06 10:35:15 +0100

    little start of pruning

--- BNReasoner.py
+++ BNReasoner.py
@@ -16,3 +16,50 @@ class BNReasoner:
             self.bn = net
 
     # TODO: This is where your methods should go
+
+    def node_prune(self, Q, e):
+
+
+
+        pass
+
+    def edge_prune(self, Q, e):
+        '''
+        Prunes the edges of the network, given query variables Q and evidence e
+        Input:
+        - Q: list of query variables
+        - e: dictionary of evidence variables with truth values
+        Output:
+        - None
+        '''
+
+        # find which edges to delete
+        edges_to_delete = []
+        for edge in self.bn.structure.edges:
+            if edge[0] == e:
+                edges_to_delete.append(edge)
+        
+        # do it in two steps to avoid changing the structure while iterating
+        for edge in edges_to_delete:
+            self.bn.del_edge(edge) 
+
+        # use bm.reduce_factor() to update the tables (not really sure how to do this)
+        
+
+
+    def network_pruning(self, Q, e):
+
+        self.edge_prune(Q, e)
+        self.node_prune(Q, e)
+
+
+if __name__ == '__main__':
+    # Load the BN from the BIFXML file
+    bn = BNReasoner('testing/dog_problem.bifxml')
+
+    # Print the interaction graph
+
+    bn.edge_prune([], 'hear-bark')
+
+
+
