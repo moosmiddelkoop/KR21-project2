d0bbea8cbc155eb7b445b876606fa6524c3b89fa
Moos <jimmemiddelkoop@gmail.com>
2022-12-14 20:37:43 +0100

    ordering strat experiment

--- experiment.py
+++ experiment.py
@@ -1,23 +1,41 @@
 import BayesNet
-import BNReasoner
+from BNReasoner import BNReasoner
+import random
+import time
 
 FILE = 'bifxml/small/asia.bifxml'
 
-class Experiment(file_path):
+class Experiment:
 
-    def __init__(self, net: Union[str, BayesNet]):
+    def __init__(self, file_path):
         """
-        :param net: either file path of the bayesian network in BIFXML format or BayesNet object
+        get reasoner object from file path
         """
-        if type(net) == str:
-            # constructs a BN object
-            self.bn = BayesNet()
-            # Loads the BN from an BIFXML file
-            self.bn.load_from_bifxml(net)
-        else:
-            self.bn = net
+        self.Reasoner = BNReasoner(file_path)
 
     # variable elimination with different orderings
-    def ordering_strategy_experiment(BN):
-        pass
+    def ordering_strategy_experiment(self, percentage_vars, ordering_strategy):
+
+        # get the variables in the BN (list of strings)
+        variables = self.Reasoner.bn.get_all_variables()
+
+        # get random sample of variables
+        sample_size = int(len(variables) * percentage_vars)
+        sample = random.sample(variables, sample_size)
+
+        start = time.time()
+
+        # eliminate the variables in the right order
+        final_cpt = self.Reasoner.var_elimination(sample, ordering_strat='min-fill')
+
+        end = time.time()
+
+        runtime = end - start
+
+        return final_cpt, runtime
+
+Experiment1 = Experiment(FILE)
+print(Experiment1.ordering_strategy_experiment(0.5, 'min_fill'))
+
+
 
